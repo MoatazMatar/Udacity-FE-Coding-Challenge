42451e78f9a06583367e24edf19654473a5d3345
Moataz Matar <mmatar@inmobly.com>
2018-10-08 12:49:44 +0200

    adding functions for login and register

--- users-crud/src/app/login/shared/login-register.service.ts
+++ users-crud/src/app/login/shared/login-register.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginRegisterService {
+  private lgoinUrl = environment.apiEndpoint + '/api/login';
+  private registerUrl = environment.apiEndpoint + '/api/register';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  login(email, password) {
+    const user = {
+      email: email,
+      password: password
+    };
+    return this.http.post(this.lgoinUrl, user);
+  }
+
+  register(email, password) {
+    const user = {
+      email: email,
+      password: password
+    };
+    return this.http.post(this.registerUrl, user);
+  }
 }
